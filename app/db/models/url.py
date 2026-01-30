a64eb0f5def7c0b5c0d484c14a9fad8eb91dec91
David Achoy <david.achoy0@gmail.com>
2026-01-29 23:56:20 -0600

    Models: Create URL model and migration

---
+++ app/db/models/url.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, DateTime, Integer
+from sqlalchemy.sql import func
+from app.db.base import Base
+
+class URL(Base):
+    __tablename__ = "urls"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    short_code = Column(String(10), unique=True, index=True, nullable=False)
+    target_url = Column(String(2048), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
